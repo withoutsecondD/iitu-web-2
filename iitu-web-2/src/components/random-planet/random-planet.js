98d1586f32446c8b2343b9432ba8e0bb6e111135
Diyar Z <marbiru15@gmail.com>
2025-02-26 09:42:39 +0500

    step 4 - added errors catcher

--- iitu-web-2/src/components/random-planet/random-planet.js
+++ iitu-web-2/src/components/random-planet/random-planet.js
@@ -4,13 +4,15 @@ import Spinner from '../spinner'
 import SwapiService from '../../services/swapi-service'
 
 import './random-planet.css'
+import ErrorIndicator from "../error-indicator";
 
 export default class RandomPlanet extends Component {
     swapiService = new SwapiService();
 
     state = {
         planet: {},
-        loading: true
+        loading: true,
+        error: false
     };
 
     constructor(props) {
@@ -25,20 +27,32 @@ export default class RandomPlanet extends Component {
         });
     };
 
+    onError = (err) => {
+        this.setState({
+            loading: false,
+            error: true
+        });
+    }
+
     updatePlanet() {
         const id = Math.floor(Math.random()*15 + 2);
         this.swapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch((err) => this.onError())
     }
 
     render() {
-        const { planet, loading } = this.state;
+        const { planet, loading, error } = this.state;
+        const hasData = !(loading || error)
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const content = !loading ? <PlanetView planet={planet}/> : null;
+        const content = hasData ? <PlanetView planet={planet}/> : null;
 
         return (
             <div className="random-planet jumbotron rounded">
+                {errorMessage}
                 {spinner}
                 {content}
             </div>
@@ -47,8 +61,7 @@ export default class RandomPlanet extends Component {
 }
 
 const PlanetView = ({ planet }) => {
-    const { id, name, population,
-        rotationPeriod, diameter } = planet;
+    const { id, name, population, rotationPeriod, diameter } = planet;
 
     return (
         <React.Fragment>
