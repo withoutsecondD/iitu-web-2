b7f1a16e443f473b5a357b5464c2a32d56e1da2c
Diyar Z <marbiru15@gmail.com>
2025-03-05 12:48:31 +0500

    step 10 - added error catcher component

--- iitu-web-2/src/components/people-page/people-page.js
+++ iitu-web-2/src/components/people-page/people-page.js
@@ -7,19 +7,13 @@ import ErrorIndicator from '../error-indicator/error-indicator';
 import './people-page.css';
 import SwapiService from "../../services/swapi-service";
 import Row from "../row";
+import ErrorBoundary from "../error-boundary";
 
 export default class PeoplePage extends Component {
     swapiService = new SwapiService();
 
     state = {
         selectedPerson: null,
-        hasError: false
-    }
-
-    componentDidCatch(error, info) {
-        this.setState({
-            hasError: true
-        })
     }
 
     onPersonSelected = (selectedPerson) => {
@@ -27,18 +21,10 @@ export default class PeoplePage extends Component {
     };
 
     render() {
-
-        if (this.state.hasError) {
-            return <ErrorIndicator />
-        }
-
         const itemList = (
-            <ItemList
-                onItemSelected={this.onPersonSelected}
-                getData={ this.swapiService.getAllPeople }
-                renderItem={({ name, gender, birthYear}) => (
-                    `${name} (${gender}, ${birthYear})`
-                )} />
+            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}>
+                { i => `${i.name} (${i.birthYear})`}
+            </ItemList>
         )
 
         const personDetails = (
@@ -46,7 +32,9 @@ export default class PeoplePage extends Component {
         )
 
         return (
-            <Row left={ itemList } right={ personDetails }/>
+            <ErrorBoundary>
+                <Row left={ itemList } right={ personDetails } />
+            </ErrorBoundary>
         )
     }
 }
