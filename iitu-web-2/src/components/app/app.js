212daff95020c52ae2ca0dfafb6a5c883b44d516
Diyar Zulyarov <marbiru15@gmail.com>
2025-03-19 11:33:35 +0500

    step 13 - refactoring

--- iitu-web-2/src/components/app/app.js
+++ iitu-web-2/src/components/app/app.js
@@ -7,6 +7,8 @@ import './app.css'
 import ErrorIndicator from "../error-indicator";
 import PeoplePage from "../people-page";
 import SwapiService from "../../services/swapi-service";
+import {PersonList, PlanetList, StarshipList} from "../sw-components";
+import ErrorBoundary from "../error-boundary";
 
 export default class App extends Component {
     swapiService = new SwapiService();
@@ -26,11 +28,24 @@ export default class App extends Component {
         }
 
         return (
-            <div>
-                <Header/>
-                <RandomPlanet />
-                <PeoplePage />
-            </div>
+            <ErrorBoundary>
+                <div>
+                    <Header/>
+                    <RandomPlanet />
+
+                    <PersonList>
+                        { i => `${i.name} (${i.birthYear})` }
+                    </PersonList>
+
+                    <StarshipList>
+                        { i => `${i.name} (${i.cargoCapacity})` }
+                    </StarshipList>
+
+                    <PlanetList>
+                        { i => `${i.name} (${i.population})` }
+                    </PlanetList>
+                </div>
+            </ErrorBoundary>
         )
     }
 };
--- iitu-web-2/src/components/item-list/item-list.js
+++ iitu-web-2/src/components/item-list/item-list.js
@@ -1,8 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react'
 
 import './item-list.css'
-import SwapiService from "../../services/swapi-service"
-import withData from "../hoc-helpers/with-data"
 
 const ItemList = (props) => {
     const {data, onItemSelected, children: renderLabel} = props
@@ -27,6 +25,4 @@ const ItemList = (props) => {
     )
 }
 
-const { getAllPeople } = new SwapiService()
-
-export default withData(ItemList, getAllPeople)
+export default ItemList
